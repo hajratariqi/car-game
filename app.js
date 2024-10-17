003010679270df9a90c4b33590761e2169e04060
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-10-18 01:16:12 +0500

    Problem fixed

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ let interval = "";
 let signalInterval = "";
 let carGif = document.getElementById('car');
 const signal = document.getElementById('signal');
+let isManualStop = false
 
-function driveCar(){  
+function driveCar(){
+     
     interval = setInterval(()=>{
         speed += 5
         carGif.style.left = speed + 'px'
@@ -22,16 +24,22 @@ signalInterval = setInterval(()=>{
     if (signal.src.endsWith('yellow.png')) {
         signal.src = './green.png';
         clearInterval(interval)
+        if(!isManualStop){
         driveCar()
+        }
 
     } else if (signal.src.endsWith('green.png')) {
-        signal.src = './red.png';
+        signal.src = './Red-light.png';
+        if(!isManualStop){
         clearInterval(interval)
+        }
     }
      else {
         signal.src = './yellow.png';
         clearInterval(interval)
+        if(!isManualStop){
         driveCar()
+        }
     }
 },2000)
 }
@@ -42,10 +50,12 @@ ChangeSignal()
 var toogle = false
 function control(){
     let btn = event.target
-    
 
     if(toogle === false){
     clearInterval(interval)
+    clearInterval(signalInterval)
+    ChangeSignal()
+    isManualStop = true
     btn.innerText = 'Start'
     btn.style.backgroundColor = 'red'
     toogle = true
@@ -53,8 +63,10 @@ function control(){
     else{
         clearInterval(interval)
         driveCar()
+        ChangeSignal()
         btn.innerText = 'Stop'
         btn.style.backgroundColor = 'green'
         toogle = false
+        isManualStop = false
     }
 }
