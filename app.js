29bfb905aead4a3201734c646a43b1165bf98306
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-10-17 12:55:38 +0500

    reset timer

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-let speed = 1
 function driveCar(){
+    let speed = 1
     const carGif = document.getElementById('car')
     const interval = setInterval(()=>{
         speed += 10
         carGif.style.left = speed + 'px'
         
-    
+        if(carGif.style.left == '971px'){
+            speed = 1
+        }
+        console.log(speed);
     },100)
 }
 driveCar()
