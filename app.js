728de4b87848f5527fa965528a9b031c7db61543
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-10-18 00:39:09 +0500

    Added trafic light

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
+let i = 0;
+let speed = 1;
 let interval = "";
-const carGif = document.getElementById('car')
-let speed = 1
-let signalInterval = ""
-let i = 0
+let signalInterval = "";
+let carGif = document.getElementById('car');
+const signal = document.getElementById('signal');
 
-function driveCar(){
+function driveCar(){  
     interval = setInterval(()=>{
         speed += 5
         carGif.style.left = speed + 'px'
-        
         if(carGif.style.left == '971px'){
             speed = 1
         }
     },20)
 }
 driveCar()
+
+
+function ChangeSignal(){
+signalInterval = setInterval(()=>{
+    if (signal.src.endsWith('yellow.png')) {
+        signal.src = './green.png';
+        clearInterval(interval)
+        driveCar()
+
+    } else if (signal.src.endsWith('green.png')) {
+        signal.src = './red.png';
+        clearInterval(interval)
+    }
+     else {
+        signal.src = './yellow.png';
+        clearInterval(interval)
+        driveCar()
+    }
+},2000)
+}
+ChangeSignal()
+
 
 
 var toogle = false
