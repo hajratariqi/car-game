5386c3b5384cbe93c82a373dd0070d6e102b38fa
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-10-17 22:11:44 +0500

    Added signal images

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
 let interval = "";
 const carGif = document.getElementById('car')
 let speed = 1
+let signalInterval = ""
+let i = 0
 
 function driveCar(){
-     interval = setInterval(()=>{
-        speed += 10
+    interval = setInterval(()=>{
+        speed += 5
         carGif.style.left = speed + 'px'
         
         if(carGif.style.left == '971px'){
             speed = 1
         }
-        console.log(speed);
     },20)
 }
 driveCar()
 
+
 var toogle = false
 function control(){
     let btn = event.target
+    
 
     if(toogle === false){
     clearInterval(interval)
     btn.innerText = 'Start'
-    btn.style.backgroundColor = 'gold'
-    btn.style.color = 'black'
+    btn.style.backgroundColor = 'red'
     toogle = true
     }
     else{
+        clearInterval(interval)
         driveCar()
         btn.innerText = 'Stop'
         btn.style.backgroundColor = 'green'
-        btn.style.color = 'white'
         toogle = false
     }
 }
